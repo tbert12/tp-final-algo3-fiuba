9526ee2ff4ac97b22b4fece8e300762eca57b03d
facurawson@gmail.com <facurawson@gmail.com@b66c32b9-9ae1-bfce-31e2-02c538bb0516>
2014-06-07 12:53:53 +0000

    Ladron Test

---  tp-final-algo3-fiuba/src/fiuba/algo3/tpfinal/clases/Ladron.java
+++  tp-final-algo3-fiuba/src/fiuba/algo3/tpfinal/clases/Ladron.java
@@ -1,5 +1,18 @@
 package fiuba.algo3.tpfinal.clases;
 
 public class Ladron {
-		//CLASE LADRON EJ
+	private String sexo,hobby,cabello,senia,vehiculo;
+	
+	public Ladron(String UnSexo,String UnHobby,String UnCabello,String UnaSenia,String UnVehiculo){
+		this.sexo = UnSexo;
+		this.hobby = UnHobby;
+		this.cabello = UnCabello;
+		this.senia = UnaSenia;
+		this.vehiculo = UnVehiculo;
+	}
+	
+	public String Sexo(){
+		return this.sexo;
+	}
+	
 }
---  tp-final-algo3-fiuba/test/fiuba/algo3/tpfinal/test/LadronTest.java
+++  tp-final-algo3-fiuba/test/fiuba/algo3/tpfinal/test/LadronTest.java
@@ -2,12 +2,15 @@ package fiuba.algo3.tpfinal.test;
 
 import org.junit.Assert;
 import org.junit.Test;
+import fiuba.algo3.tpfinal.clases.Ladron;
+
 
 public class LadronTest {
 
 	@Test
-	public void test() {
-		Assert.assertEquals("T","T");
+	public void LadronEsFemenino() {
+		Ladron ladron = new Ladron("femenino","","","","");
+		Assert.assertEquals(ladron.Sexo(),"femenino");
 	}
 
 }
