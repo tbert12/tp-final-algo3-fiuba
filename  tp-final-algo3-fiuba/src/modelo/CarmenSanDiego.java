2a9b105a2805c890d73eef4279f4e1cfe7b151fe
octaviomdq93@gmail.com <octaviomdq93@gmail.com@b66c32b9-9ae1-bfce-31e2-02c538bb0516>
2014-06-18 17:06:48 +0000

    Terminado y probado todo lo que es serializacion y XML de ladrones. Listo para usar.

---  tp-final-algo3-fiuba/src/modelo/CarmenSanDiego.java
+++  tp-final-algo3-fiuba/src/modelo/CarmenSanDiego.java
@@ -26,9 +26,11 @@ import org.xml.sax.SAXException;
 
 public  class CarmenSanDiego {
 	private static String nombreArchivoPolicias = "RegistroPolicias.xml";
+	private static String nombreArchivoLadrones = "RegistroLadrones.xml";
 	public static List<Policia> ListadoPolicias = new ArrayList<Policia>();
 	public static List<Ladron> ListadoLadrones = new ArrayList<Ladron>();
-	public static int CantidadDePolicias = ListadoPolicias.size();
+	public static int CantidadDePolicias = 0;
+	public static int CantidadDeLadrones = 0;
 	
 	public void LevantarPoliciasDelXML() throws ParserConfigurationException, SAXException, IOException{
 		File archivo = new File(nombreArchivoPolicias);
@@ -42,15 +44,39 @@ public  class CarmenSanDiego {
 				Policia policia = Policia.Hidratar(doc,i);
 				ListadoPolicias.add(policia);
 			}
-			}
-	
+		}
 	
 	}
-
-	public void BajarPoliciasAXML() throws ParserConfigurationException, TransformerException{
+	public void LevantarLadronesDelXML() throws ParserConfigurationException, SAXException, IOException{
+		File archivo = new File(nombreArchivoLadrones);
+		if (archivo.exists()){
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+			Document doc = dBuilder.newDocument();
+			doc = dBuilder.parse(archivo);
+			doc.getDocumentElement().normalize();
+			for (int i = 0;i < CantidadDeLadrones;i++){
+				Ladron ladron = Ladron.ladronHidratar(doc,i);
+				ListadoLadrones.add(ladron);
+			}
+		}
+	}
+	private Document crearDoc() throws ParserConfigurationException{
 		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
 		DocumentBuilder db = dbf.newDocumentBuilder();
 		Document doc = db.newDocument();
+		return doc;
+	}
+	private void TransformarYEscribirADisco(String nombreArchivo, Document doc) throws TransformerException{
+		TransformerFactory transformerFactory = TransformerFactory.newInstance();
+		Transformer transformer = transformerFactory.newTransformer();
+		DOMSource source = new DOMSource(doc);
+		File archivoDestino = new File(nombreArchivo);
+		StreamResult result = new StreamResult(archivoDestino);
+		transformer.transform(source, result);
+	}
+	public void BajarPoliciasAXML() throws ParserConfigurationException, TransformerException{
+		Document doc = crearDoc();
 		for (Policia policia:ListadoPolicias){
 			Element policiaSerializado = policia.Serializar(doc);
 			if (doc.hasChildNodes()==false){//Creo la root
@@ -60,26 +86,49 @@ public  class CarmenSanDiego {
 			doc.getFirstChild().appendChild(policiaSerializado);
 			}
 			}
-		TransformerFactory transformerFactory = TransformerFactory.newInstance();
-		Transformer transformer = transformerFactory.newTransformer();
-		DOMSource source = new DOMSource(doc);
-		File archivoDestino = new File(nombreArchivoPolicias);
-		StreamResult result = new StreamResult(archivoDestino);
-		transformer.transform(source, result);
+		TransformarYEscribirADisco(nombreArchivoPolicias,doc);
 		}
+	public void BajarLadronesAXML() throws ParserConfigurationException, TransformerException{
+		Document doc = crearDoc();
+		for (Ladron ladron:ListadoLadrones){
+			Element ladronSerializado = ladron.serializarLadron(doc);
+			if (doc.hasChildNodes()==false){
+				doc.appendChild(ladronSerializado);
+			}
+			else{
+				doc.getFirstChild().appendChild(ladronSerializado);
+			}
+		TransformarYEscribirADisco(nombreArchivoLadrones,doc);
+		}
+	}
 
 	public void AgregarPolicia(Policia unPolicia){
 		ListadoPolicias.add(unPolicia);
+		CantidadDePolicias++;
+	}
+	public void AgregarLadron(Ladron unLadron){
+		ListadoLadrones.add(unLadron);
+		CantidadDeLadrones++;
+	}
+	public Boolean LadronEstaEnJuego(Ladron unLadron){
+		return ListadoLadrones.contains(unLadron);
 	}
 	public Boolean PoliciaEstaEnJuego(Policia unPolicia){
 		return ListadoPolicias.contains(unPolicia);
 	}
-	public Boolean ArchivoPoliciasExiste(){
-		File archivo = new File(nombreArchivoPolicias);
+	private Boolean ArchivoExiste(String nombreArchivo){
+		File archivo = new File(nombreArchivo);
 		if (archivo.exists()){
 			return true;
 		}
 		return false;
 	}
+	public Boolean ArchivoPoliciasExiste(){
+		return ArchivoExiste(nombreArchivoPolicias);
+	}
+	public Boolean ArchivoLadronesExiste(){
+		return ArchivoExiste(nombreArchivoLadrones);
+	}
+	
 	}
 
---  tp-final-algo3-fiuba/test/fiuba/algo3/tpfinal/test/CarmenSanDiegoTest.java
+++  tp-final-algo3-fiuba/test/fiuba/algo3/tpfinal/test/CarmenSanDiegoTest.java
@@ -17,9 +17,13 @@ import modelo.*;
 public class CarmenSanDiegoTest {
 	@After
 	public void after() {
-		File archivo = new File("RegistroPolicias.xml");
-		if (archivo.exists()) {
-			archivo.delete();
+		File archivo1 = new File("RegistroPolicias.xml");
+		if (archivo1.exists()) {
+			archivo1.delete();
+		}
+		File archivo2 = new File("RegistroLadrones.xml");
+		if (archivo2.exists()){
+			archivo2.delete();
 		}
 	}
 	@Test
@@ -32,6 +36,16 @@ public class CarmenSanDiegoTest {
 		Assert.assertTrue(Carmen.PoliciaEstaEnJuego(unPolicia));
 		Assert.assertTrue(Carmen.PoliciaEstaEnJuego(otroPolicia));
 	}
+	@Test
+	public void CarmenCargaLadrones(){
+		CarmenSanDiego Carmen = new CarmenSanDiego();
+		Ladron LadronUno = new Ladron("Tito",null);
+		Ladron LadronDos = new Ladron("Jose",null);
+		Carmen.AgregarLadron(LadronUno);
+		Carmen.AgregarLadron(LadronDos);
+		Assert.assertTrue(Carmen.LadronEstaEnJuego(LadronUno));
+		Assert.assertTrue(Carmen.LadronEstaEnJuego(LadronDos));
+	}
 	
 	@Test
 	public void CarmenCreaXMLConPolicias() throws ParserConfigurationException, TransformerException{
@@ -42,6 +56,33 @@ public class CarmenSanDiegoTest {
 		Carmen.AgregarPolicia(otroPolicia);
 		Carmen.BajarPoliciasAXML();
 		Assert.assertTrue(Carmen.ArchivoPoliciasExiste());
+	}
+	@Test
+	public void CarmenCreaXMLConLadrones() throws ParserConfigurationException, TransformerException{
+		CarmenSanDiego Carmen = new CarmenSanDiego();
+		Caracteristicas caracteristicas1=new Caracteristicas(null,null,null,null,null);
+		Caracteristicas caracteristicas2=new Caracteristicas(null,null,null,null,null);
+		Ladron LadronUno = new Ladron("Tito",caracteristicas1);
+		Ladron LadronDos = new Ladron("Jose",caracteristicas2);
+		Carmen.AgregarLadron(LadronUno);
+		Carmen.AgregarLadron(LadronDos);
+		Carmen.BajarLadronesAXML();
+		Assert.assertTrue(Carmen.ArchivoLadronesExiste());
+	
+	}
+	@Test
+	public void CarmenLevantaXMLConLadrones() throws ParserConfigurationException, TransformerException, SAXException, IOException{
+		CarmenSanDiego Carmen = new CarmenSanDiego();
+		Caracteristicas caracteristicas1=new Caracteristicas(null,null,null,null,null);
+		Caracteristicas caracteristicas2=new Caracteristicas(null,null,null,null,null);
+		Ladron LadronUno = new Ladron("Tito",caracteristicas1);
+		Ladron LadronDos = new Ladron("Jose",caracteristicas2);
+		Carmen.AgregarLadron(LadronUno);
+		Carmen.AgregarLadron(LadronDos);
+		Carmen.BajarLadronesAXML();
+		Carmen.LevantarLadronesDelXML();
+		Assert.assertTrue(Carmen.LadronEstaEnJuego(LadronUno));
+		Assert.assertTrue(Carmen.LadronEstaEnJuego(LadronDos));
 	}
 	@Test
 	public void CarmenLevantaXMLConPolicias() throws ParserConfigurationException, TransformerException, SAXException, IOException{
